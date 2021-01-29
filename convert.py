36bdce46556d15eb83510ffe89633ed7630220bc
Arthur <arthur.pointeau@gmail.com>
2021-01-29 17:33:41 +0100

    insert rating into file

--- convert.py
+++ convert.py
@@ -1,14 +1,20 @@
 import csv
 import geojson
 
-with open('data.csv') as csvfile:
-     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-     for row in spamreader:
-         print(', '.join(row))
-
-
-import geojson
 with open('world-110m.geojson') as f:
     gj = geojson.load(f)
 
-features = gj['features'][0]
+features = gj['features']
+
+with open('data.csv') as csvfile:
+     spamreader = csv.reader(csvfile, delimiter=',')
+     for row in spamreader:
+         for feature in features:
+             if row[0] == feature['id']:
+                 feature['properties']['rating'] = row[2]
+
+
+gj['features'] = features
+
+with open('out.geosjon', 'w') as outfile:
+    geojson.dump(gj, outfile)
