6ed7ee0dfdfa1a7c06ab8bfe57e2a864a53b4253
Arthur <arthur.pointeau@gmail.com>
2021-01-31 16:27:19 +0100

    update

--- convert.py
+++ convert.py
@@ -10,7 +10,7 @@ with open('data.csv') as csvfile:
      spamreader = csv.reader(csvfile, delimiter=',')
      for row in spamreader:
          for feature in features:
-             if row[0] == feature['id']:
+             if row[0] == feature['properties']['name']:
                  feature['properties']['rating'] = row[2]
 
 
